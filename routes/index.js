158ff299ca59a563fa6e75f16ddc2adf50dc08c9
Andrew Blejde <snehp491@gmail.com>
2022-08-03 02:42:02 -0400

    add in model column definition

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
