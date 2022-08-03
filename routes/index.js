3ff360b6d8eefa01dd804da0ef8343697c059ea2
Andrew Blejde <snehp491@gmail.com>
2022-08-03 02:43:09 -0400

    undo failed refactor

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
